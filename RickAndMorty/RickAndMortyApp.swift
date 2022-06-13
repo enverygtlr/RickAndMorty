e15d34e4f0ab9168b301b57fe76d6f2628010949
enverygtlr <36340901+enverygtlr@users.noreply.github.com>
2022-06-13 12:11:00 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Enver Yigitler on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
